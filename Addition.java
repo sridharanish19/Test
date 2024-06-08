60a96f22accb4b765b892e1c80aa853e13bdcad7
sridharanish19 <sridharanish19@gmail.com>
2024-06-08 12:34:25 -0500

    Second commit

---
+++ Addition.java
@@ -0,0 +1,10 @@
+public class Addition {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        System.out.println("Number 1: "+a);
+        System.out.println("Number 2: "+b);
+        int result = a + b;
+        System.out.println("Addition of two numbers is: "+result);
+    }
+}
